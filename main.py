854512a66f2b9f9ee61fba1aa939d9eea4f0f9ca
SantiagoHernandez12 <angelapineda413@gmail.com>
2025-04-28 19:21:33 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,59 @@
-import random
-characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+import discord
+from discord.ext import commands
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+@bot.event
+async def on_ready():
+    print(f'Tu bot {bot.user} esta en linea!')
+    
+@bot.command()
+async def saludar(ctx,*, mensaje:str):
+    
+    mensaje = mensaje.lower().strip()
+    
+    if mensaje == 'hola':
+        
+        await ctx.send('Hola, ¿cómo estás?')
+        
+    elif mensaje == 'chao':
+        
+        await ctx.send('bye')
+    
+    elif mensaje == 'la buena':
+        
+        await ctx.send('la buena tambien')
 
-length = int(input("¿De cuántos caracteres quieres tu contraseña?: "))
+        import discord
+from discord.ext import commands
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+@bot.event
+async def en_linea():
+    print(f'Tu bot {bot.user} esta en linea!')
+    
+@bot.command()
+async def saludar(ctx,*, mensaje:str):
+    
+    mensaje = mensaje.lower().strip()
+    
+    if mensaje == 'hola':
+        
+        await ctx.send('Hola, ¿cómo estás?')
+        
+    elif mensaje == 'chao':
+        
+        await ctx.send('bye')
+    
+    elif mensaje == 'la buena':
+        
+        await ctx.send('la buena tambien')
 
-password = ""
-for i in range(length):
-    password = password + random.choice(characters)
-    print(f'tu contraseña es{password}')
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a + b)
+
+        
+token = ''
+bot.run(token)
